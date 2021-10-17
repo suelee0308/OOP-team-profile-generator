6e972b1a212e7d7e3cf4443ca90ebefd25934980
Sue Lee <sue.lee0308@gmail.com>
2021-10-16 22:18:28 -0500

    inquirer works. html generates. needs css

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHTML = require('./generateHTML');
+const generateHTML = require('./lib/generateHTML');
 const Engineer = require('./lib/engineer');
 const Manager = require('./lib/manager');
 const Intern = require('./lib/intern');
@@ -8,51 +8,72 @@ const Intern = require('./lib/intern');
 const allEmployees = [];
 
 //   questions inquirer will ask 
+const confirmInput = (input) => {
+    if (input === "") {
+      return "Please do not leave section blank";
+    } else {
+      return true;
+    }
+  };
+
 const employeeQuestions = [
+    {
+    type: 'list',
+    name: 'role',
+    message: "Please select one of the following roles to add to the team:",
+    choices: ['Manager', 'Engineer', 'Intern'],
+    },
     {
     type: 'input',
     name: 'name',
     message: "What is the employee's name?",
+    validate: confirmInput,
     },
     {
     type: 'input',
     name: 'id',
     message: "What is the employee's id?",
+    validate: confirmInput,
     },
     {
     type: 'input',
     name: 'email',
     message: "What is the employee's email?",
+    validate: confirmInput,
     },
     {
-    type: 'list',
-    name: 'employeeType',
-    message: "Is the employee a manager, engineer, or intern?",
-    choices: ['Manager', 'Engineer', 'Intern'],
-    },
-    {
-    when: (data) => data.employeeType === "Manager",
+    when: (data) => data.role === "Manager",
     type: 'input',
     name: 'office',
     message: "What is the manager's office number?",
+    validate: confirmInput,
     },
     {
-    when: (data) => data.employeeType === "Engineer",
+    when: (data) => data.role === "Engineer",
+    validate: confirmInput,
     type: 'input',
     name: 'github',
     message: "What is the engineer's github username?",
+    validate: confirmInput,
     },
     {
-    when: (data) => data.employeeType === "Intern",
+    when: (data) => data.role === "Intern",
     type: 'input',
     name: 'school',
     message: "What school does the intern attend?",
+    validate: confirmInput,
+    },
+    {
+    type: 'list',
+    name: 'anotherOne',
+    message: "Would you like to create another employee?",
+    choices: ['Yes', 'No'],
     }
 ];
 
 // function to push all answer into allAnswers array
 function pushAnswers(input) {
-    switch (input.employeeType) {
+    switch (input.role) {
         case "Manager" : {
         allEmployees.push(new Manager(input.name, input.id, input.email, input.office));
         break;
@@ -68,19 +89,35 @@ function pushAnswers(input) {
     }
 }
 
-// function to write HTML file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, generateHTML(data))
-}
-
-// function to initialize app
 function init() {
     inquirer.prompt(employeeQuestions)
-    .then((userResponse) => {
-        pushAnswers(userResponse);
-        writeToFile("./output/generatedHTML", userResponse)
+    .then((input) => {
+        if (input.anotherOne === "Yes") {
+            pushAnswers(input);
+            init();
+        }else {
+            pushAnswers(input);
+            renderCSS();
+            getTeam();
+        }
     })
 };
+
+function getTeam() {
+    const html = generateHTML(allEmployees);
+    fs.writeFile("./output/generatedHTML.html", html, (err) => {
+        if (err) throw err;
+        console.log("Success! Your team has been created!");
+    })
+}
+
+function renderCSS() {
+    const css = generateCSS();
+    fs.writeFile("./output/generatedCSS.css", css, (err) => {
+        if (err) throw err;
+        console.log("Success! Your application has been styled!");
+    })
+}
 
 // Function call to initialize app
 init();
